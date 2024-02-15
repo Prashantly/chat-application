64864462bcdc1e72519449919312cc92346d82bc
Prashant Yallatti <99544800+Prashantly@users.noreply.github.com>
2024-02-15 20:08:48 +0530

    complete text and collapse feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import { useState, useEffect } from "react";
 import Window from "./components/chatwindow/Window";
+import io from "socket.io-client";
+
+const socket = io('http://localhost:3001')
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('receive_message', (data) => {
+      setMessages([...messages, { content: data.message, type: 'incoming' }])
+    })
+
+  }, [messages])
+
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && inputValue.trim() !== '') {
+      socket.emit("send_message", { message: inputValue })
+      setMessages([...messages, { content: inputValue, type: 'outgoing' }])
+      setInputValue('')
+    }
+  }
+
+  const sendMessage = () => {
+    //emit some event so that other client can listen to this event
+    // "send_message" is an event
+    if (inputValue.trim() !== '') {
+      socket.emit("send_message", { message: inputValue })
+      setMessages([...messages, { content: inputValue, type: 'outgoing' }])
+      setInputValue('')
+    }
+  }
+
   return (
-    <Window />
+    <Window
+      inputValue={inputValue}
+      setInputValue={setInputValue}
+      sendMessage={sendMessage}
+      messages={messages}
+      handleEnter={handleEnter}
+    />
   );
 }
 
--- src/components/chatwindow/Window.js
+++ src/components/chatwindow/Window.js
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from "./chat.module.css"
+import 'animate.css';
 
-const Window = () => {
+
+const Window = ({ inputValue, setInputValue, sendMessage, messages, handleEnter }) => {
+    const [collapse, setCollapase] = useState(true);
+    const chatBodyRef = useRef(null);
+
+    useEffect(() => {
+        if (chatBodyRef.current.scrollHeight > 0) {
+            chatBodyRef.current.scrollTop = chatBodyRef.current.scrollHeight;
+        }
+    }, [messages])
+
+    function toggleCollapse() {
+        setCollapase(!collapse);
+    }
+
     return (
-        <div className={styles.container}>
-            <div className={styles.chatHeader}>
+        <div className={`${styles.container} ${collapse ? styles.collapse : ''}`}>
+            <div className={styles.chatHeader} onClick={toggleCollapse}>
                 <div className={styles.logo}>
                     <img src="https://cdn-icons-png.flaticon.com/512/14292/14292687.png" alt="cwt" />
                 </div>
                 <div className={styles.chatTitle}>Let's Chat</div>
             </div>
-            <div className={styles.chatBody}>Message here</div>
+            <div className={styles.chatBody} ref={chatBodyRef}>
+                {messages.map((message, index) => (
+                    <div key={index} className={message.type === 'outgoing' ? `${styles.outgoing} animate__animated animate__fadeInLeft animate__faster` : `${styles.incoming} animate__animated animate__fadeInRight animate__faster`}>{message.content}</div>
+                ))}
+            </div>
             <div className={styles.chatInput}>
                 <div className={styles.inputSection}>
-                    <input type="text" id={styles.textInput} autoFocus placeholder='Type here...' />
+                    <input type="text"
+                        id={styles.textInput}
+                        value={inputValue}
+                        autoFocus
+                        placeholder='Type here...'
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={handleEnter}
+                    />
                 </div>
-                <div className={styles.send}>
+                <div className={styles.send} onClick={sendMessage}>
                     <img src="https://cdn-icons-png.flaticon.com/512/11105/11105622.png" height={30} width={40} alt="send" />
                 </div>
             </div>
